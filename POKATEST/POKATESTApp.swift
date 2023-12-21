6f7b33b4e59bea841ba55ea807d26035954096fa
sanju-aeturnum <71439047+sanju-aeturnum@users.noreply.github.com>
2023-12-21 17:52:16 +0530

    Initial Commit

---
+++ POKATEST/POKATESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  POKATESTApp.swift
+//  POKATEST
+//
+//  Created by Sanju-Maduwantha on 2023-12-21.
+//
+
+import SwiftUI
+
+@main
+struct POKATESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
